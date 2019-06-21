4fcb59150c73e2e8171cb64c1375af12e18ff439
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-21 15:55:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     holidays.each do |holiday, supplies|
       supplies = supplies.join(", ")
-      holiday.each do 
+      binding.pry
+        
     end
   end
 end
