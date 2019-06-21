fec960327f2beeebda7484ada1076b81717cdadb
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-21 15:29:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,8 +56,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.dig
-     
+  a = holiday_hash.dig
+     a
 end
 
 def all_holidays_with_bbq(holiday_hash)
